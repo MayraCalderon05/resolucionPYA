bc7d97273e30a23944288dbc087ca4209614b5fc
Mayra_Calderon <118376323+MayraCalderon05@users.noreply.github.com>
2025-04-15 11:02:41 -0300

    Add files via upload
    CREACION DE REPOSITORIO CREO NI IDEA

---
+++ Calderon/ejercicios_clase/comedero_gatos.java
@@ -0,0 +1,21 @@
+package ejercicios_clase;
+
+import java.util.Scanner;
+
+public class comedero_gatos {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        float peso_gato,cantidad_alimento;
+        int dias_alimentar;
+
+        System.out.println("Ingresa el peso del gato");
+        peso_gato = sc.nextFloat();
+
+        System.out.println("Ingresa los dias que va a alimentarlo");
+        dias_alimentar = sc.nextInt();
+        
+
+    }
+}
